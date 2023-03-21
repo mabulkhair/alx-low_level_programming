5fc3c3bf65a767ab6ea2b0e123c69e326be5b07e
Mohamed Aulkhair <mohamed.fathy.abulkhair@gmail.com>
2023-03-20 18:00:51 -0700

    My first program :)

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,15 +3,16 @@
 /**
  * main - Entry point
  *
- * Description: printing output
+ * Description: print a qoute using write function
+ * 	ssize_t write(int fd, const void *buf, size_t count);
  *
- * Return: 1 (Fail)
+ * Return: 1 (error)
  */
 
 
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1. quo, 59);
 	return (1);
