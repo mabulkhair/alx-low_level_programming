7a70e235da022ae913c6a2bc00cba1a0eabbad6f
Mohamed Aulkhair <mohamed.fathy.abulkhair@gmail.com>
2023-03-30 18:17:55 -0700

    PAS.

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -6,7 +6,7 @@ int main(void)
 	    int a[5];
 	      int *p;
 
- a[2] = 1024;
+	        a[2] = 1024;
 		  p = &n;
 		    /*
 		     *    * write your line of code here...
@@ -19,5 +19,5 @@ int main(void)
 		    *(p + 5) = 98;
 		      /* ...so that this prints 98\n */
 		      printf("a[2] = %d\n", a[2]);
- return (0);
+		        return (0);
 }
