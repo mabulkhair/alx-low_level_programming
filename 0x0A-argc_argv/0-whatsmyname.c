2a2fb40380aadb50befdc4da5f90409240344fbc
Mohamed Aulkhair <mohamed.fathy.abulkhair@gmail.com>
2023-04-10 03:46:13 -0700

    arg

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - prints program name, followed by  a new line
@@ -8,7 +7,7 @@
  * Return: 0
  */
 
-int main (int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	puts(argv[0]);
 	return (0);
