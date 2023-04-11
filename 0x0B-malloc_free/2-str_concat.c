f2d0a898b065983f9e7ec3eda0693f70d852c6ee
Mohamed Aulkhair <mohamed.fathy.abulkhair@gmail.com>
2023-04-11 16:27:17 -0700

    arg

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,11 +3,11 @@
 #include <string.h>
 
 /**
- *  *str_concat - concatenates two strings
- *   * @s1: string to concatenate
- *    * @s2: other string to concatenate
- *     * Return: pointer to the new string created (Success), or NULL (Error)
- *      */
+ * *str_concat - concatenates two strings
+ * @s1: string to concatenate
+ * @s2: other string to concatenate
+ * Return: pointer to the new string created (Success), or NULL (Error)
+ */
 
 char *str_concat(char *s1, char *s2)
 {
