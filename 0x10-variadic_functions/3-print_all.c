10ddfecbcd454bb846077dd0bab50fab93073c2a
Mohamed Aulkhair <mohamed.fathy.abulkhair@gmail.com>
2023-04-20 04:45:26 -0700

    Variadic Functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -28,7 +28,7 @@ void format_int(char *separator, va_list ap)
  */
 void format_float(char *separator, va_list ap)
 {
-	        printf("%s%f", separator, va_arg(ap, double));
+	printf("%s%f", separator, va_arg(ap, double));
 }
 
 /**
@@ -40,10 +40,9 @@ void format_string(char *separator, va_list ap)
 {
 	char *str = va_arg(ap, char *);
 
-
 	switch ((int)(!str))
-	case 1:
-		str = "(nil)";
+		case 1:
+			str = "(nil)";
 
 	printf("%s%s", separator, str);
 }
@@ -57,18 +56,18 @@ void print_all(const char * const format, ...)
 	| int i = 0, j;
 	char *separator = "";
 	va_list ap;
-	token_t tokens [] = {
+	token_t tokens[] = {
 		{"c", format_char},
 		{"i", format_int},
 		{"f", format_float},
 		{"s", format_string},
-		{NULL , NULL}
+		{NULL, NULL}
 	};
 
 	va_start(ap, format);
 	| while (format && format[i])
 	{
-		j =0;
+		j = 0;
 		while (tokens[j].token)
 		{
 			if (format[i] == tokens[j].token[0])
